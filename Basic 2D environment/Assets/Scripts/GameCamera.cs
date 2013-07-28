c31e31a160bd8df1beb445e189649356f22f528f
amatteson89 <amatteson89@gmail.com>
2013-07-28 13:25:08 -0700

    2d environ

---
+++ Basic 2D environment/Assets/Scripts/GameCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+	
+	public float trackSpeed;
+	
+	private Transform target;
+	
+	
+	public void setTarget(Transform t){
+		target = t;
+	}
+	
+	//Same as update method except it happens after all the other update methods
+	void LateUpdate(){
+		if(target){
+			float x = IncrementTowards(transform.position.x, target.position.x, trackSpeed);
+			float y = IncrementTowards(transform.position.y, target.position.y, trackSpeed);
+			transform.position = new Vector3(x,y, transform.position.z);
+		}
+	}
+	
+	//Incrase n towards target by speed
+	private float IncrementTowards(float n, float target, float speed){
+		if(n == target){
+			return n;	
+		} else {
+			//must n be increased or decreased to get closer to target
+			float dir = Mathf.Sign(target - n); 	
+			n += speed * Time.deltaTime * dir;
+			
+			// if n has now passed target then return target, otherwise return n
+			return (dir == Mathf.Sign(target - n))? n : target;
+		}
+	}
+	
+}
